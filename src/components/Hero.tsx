59f9888a401c9f10b05cbdcb2ec16e62264302da
nakatatsu77 <nakatatsu1177@gmail.com>
2023-12-08 23:08:35 +0900

    Hero.tsx作成

---
+++ src/components/Hero.tsx
@@ -0,0 +1,34 @@
+export function Hero() {
+  return (
+    <>
+      <section className="w-full min-h-screen bg-root-hero-sp md:bg-root-hero-pc bg-cover bg-center flex justify-center items-center">
+        <div className="w-[328px] md:w-2/3   flex-col justify-start items-start gap-16 flex">
+          <div className="flex-col gap-6 flex">
+            <h2 className="text-neutral-800 text-2xl md:text-5xl font-bold font-['Noto Sans JP'] leading-9 md:leading-[72px] tracking-wide">
+              テクノロジーを通じて、
+              <br />
+              夢や目標を実現できる
+              <br />
+              環境を提供します
+            </h2>
+            <h3 className="text-zinc-800 text-base md:text-xl font-medium font-['Noto Sans JP'] leading-normal md:leading-[30px] tracking-wide">
+              IT教育、システム開発から
+              <br className="md:hidden" />
+              企業を支援する
+              <br />
+              テクノロジーカンパニーです。
+            </h3>
+          </div>
+          <a
+            className="w-60 md:w-80 h-14 md:h-[78px] px-8 md:px-12 py-4 md:py-6 bg-lime-300 rounded-full justify-center items-center inline-flex"
+            href=""
+          >
+            <button className="text-center text-neutral-800 text-base md:text-xl font-bold font-['Noto Sans JP'] leading-normal md:leading-[30px] tracking-wide">
+              事業内容を見る
+            </button>
+          </a>
+        </div>
+      </section>
+    </>
+  );
+}
