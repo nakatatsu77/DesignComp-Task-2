6a29068c9754c68d951742e845fae55990a4874f
nakatatsu77 <nakatatsu1177@gmail.com>
2023-12-13 17:16:41 +0900

    worksコンポーネントの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
+import { Cta } from "@/components/Cta";
 import { Hero } from "@/components/Hero";
-
+import { Service } from "@/components/Service";
+import { Works } from "@/components/Works";
 
 export default function Home() {
   return (
     <>
       <main>
         <Hero />
+        <Service />
+        <Works />
+        <Cta />
       </main>
     </>
   );
